aa2b08cee7f0847d9bbae53c7ac73a0c9b47d05a
naimkim <naimkim2000@naver.com>
2020-10-15 16:30:53 +0900

    06_

--- main.c
+++ main.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int all_files;
+
+extern void sub();
 
 int main(int argc, char *argv[]) {
-	int i;
 	
-	for ( i=0; i<5; i++)
-	{
-		static int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
-	
+	sub();
+	printf("%d\n", all_files);	
 	return 0;
 }
